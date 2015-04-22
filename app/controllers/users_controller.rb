c40157bcd18a9141698d7a77c1d8ae3b1e61dbbf
kyleconkright <kyleconkright@gmail.com>
2015-04-21 18:43:30 -0700

    added form validation for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:username, :email, :first_name, :last_name, :password)
+		params.require(:user).permit(:username, :email, :first_name, :last_name, :password, :password_confirmation)
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,26 @@
 class User < ActiveRecord::Base
-	has_many :lists
-	has_many :records, through: :lists
+  has_many :lists
+  has_many :records, through: :lists
 
-	attr_reader :password
-	
-	def password=(unencrypted_password)
-		unless unencrypted_password.empty?
-			@password = unencrypted_password
-			self.password_digest = BCrypt::Password.create(unencrypted_password)
-		end
-	end
+  attr_reader :password
 
-	def authenticate(unencrypted_password)
-		if BCrypt::Password.new(self.password_digest) == unencrypted_password
-			return self
-		else
-			return false
-		end
-	end
+  def password=(unencrypted_password)
+    unless unencrypted_password.empty?
+      @password = unencrypted_password
+      self.password_digest = BCrypt::Password.create(unencrypted_password)
+    end
+  end
+
+  def authenticate(unencrypted_password)
+    if BCrypt::Password.new(self.password_digest) == unencrypted_password
+      return self
+      else
+      return false
+    end
+  end
+
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :password, presence: true, confirmation: true, length: {in: 6..20}
 
 end
